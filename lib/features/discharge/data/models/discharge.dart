7c8354fb8948f9f6ec6a150c4a96148971383645
aliakrem <barka.aliakrem@gmail.com>
2025-06-30 09:59:53 +0100

    fix: parse response to boolean

--- lib/features/discharge/data/models/discharge.dart
+++ lib/features/discharge/data/models/discharge.dart
@@ -15,11 +15,11 @@ class StudentDischarge {
 
   factory StudentDischarge.fromJson(Map<String, dynamic> json) {
     return StudentDischarge(
-      sitBc: (json['sitBc'] as int?) == 1,
-      sitBrs: (json['sitBrs'] as int?) == 1,
-      sitDep: (json['sitDep'] as int?) == 1,
-      sitBf: (json['sitBf'] as int?) == 1,
-      sitRu: (json['sitRu'] as int?) == 1,
+      sitDep: toBool((json['sitDep'])),
+      sitBf: toBool(json['sitBf']),
+      sitBc: toBool(json['sitBc']),
+      sitRu: toBool(json['sitRu']),
+      sitBrs: toBool(json['sitBrs']),
     );
   }
 
@@ -32,4 +32,11 @@ class StudentDischarge {
       'sitBrs': sitBrs,
     };
   }
+}
+
+bool toBool(dynamic value) {
+  if (value is bool) return value;
+  if (value is int) return value == 1;
+  if (value is String) return value.toLowerCase() == 'true' || value == '1';
+  return false; // default fallback
 }
